ecac9afe5de36f2afe571252485a4b8511bc81ad
carl <cdrw2400@gmail.com>
2014-11-07 13:29:15 +0000

    the next stage of evolutiom

--- releng/airootfs/root/customize_airootfs.sh
+++ releng/airootfs/root/customize_airootfs.sh
@@ -10,15 +10,15 @@ ln -sf /usr/share/zoneinfo/UTC /etc/localtime
 usermod -s /bin/bash root
 cp -aT /etc/skel/ /root/
 
-useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash furch
+useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/bash evo
 
 chmod 750 /etc/sudoers.d
 chmod 440 /etc/sudoers.d/g_wheel
-chown -R furch:users /home/furch
+chown -R evo:users /home/evo
 
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 sed -i 's/#\(Storage=\)auto/\1volatile/' /etc/systemd/journald.conf
 
 systemctl enable pacman-init.service choose-mirror.service
 systemctl set-default multi-user.target
-#uuidgen | { read; echo "${REPLY//-}">/etc/machine-id; }
+
